7c2449b5ad61bdb60c9b21793cabe3f603c26702
creatorbox56 <46477899+creatorbox56@users.noreply.github.com>
2023-05-01 11:17:16 +0200

    make it better9

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 
 app.use(express.static(path.join("/var/www/cboxapp/", 'build')));
 
@@ -8,4 +10,11 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join("/var/www/cboxapp/", 'build', 'index.html'));
 });
 
-app.listen(9000);
+// we will pass our 'app' to 'https' server
+https.createServer({
+  key: fs.readFileSync('/root/cert/' + 'key.pem', 'utf8'),
+  cert: fs.readFileSync('/root/cert/' + 'cert.pem', 'utf8')
+}, app)
+.listen(443);
+
+//app.listen(80);
